0f3d7c667cc55e3096403333a3dfad0f345db35f
nineDays2019 <night_days@163.com>
2021-04-01 16:11:39 +0800

    📺 SlowCut Init

---
+++ slow-cut/src/main/java/me/juhezi/slowcut/CLActivity.kt
@@ -0,0 +1,14 @@
+package me.juhezi.slowcut
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * 命令行页面
+ */
+class CLActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_clactivity)
+    }
+}
