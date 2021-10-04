1f462bd50d7ee79009d3a2b0311e860cb6d0e0c0
nineDays2019 <night_days@163.com>
2021-10-04 21:02:15 +0800

    change module name

--- slow-cut/src/main/java/me/juhezi/slowcut/CLActivity.kt
+++ slow-cut/src/main/java/me/juhezi/slowcut/CLActivity.kt
@@ -15,13 +15,5 @@ class CLActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_clactivity)
-        /*with(window.decorView as ViewGroup) {
-            val root = get(0)
-            removeView(root)
-            val wrapper = RelativeLayout(context)
-            wrapper.addView(root)
-            addView(wrapper)
-        }*/
-        findViewById<View>(R.id.vg_demo).translationY = -100f
     }
 }
